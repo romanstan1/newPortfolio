5da8ff5d06458be4dcec5b251adbeb992acd22e0
Roman Stankiewicz <roman.stan3@gmail.com>
2017-06-26 10:20:53 +0100

    slide function jquery, images added

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,25 @@
 console.log('JS loaded');
+
+$(document).ready(() => {
+  const $aboutTag = $("h3.about");
+  const $myName = $("div.myName");
+  const $aboutText = $("div.aboutMe");
+
+  let aboutBoolean = false;
+
+
+
+  $aboutTag.click(function() {
+    if (!aboutBoolean) {
+      aboutBoolean = true;
+      $myName.css({"transform": "translateY(-52%) translateX(-100%)"});
+      $aboutText.css({"transform": "translateX(0%)"});
+    } else {
+      aboutBoolean = false;
+      $myName.css({"transform": "translateY(-52%) translateX(0%)"});
+      $aboutText.css({"transform": "translateX(160%)"});
+    }
+
+  });
+
+});
