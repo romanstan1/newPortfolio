0a7d4ab8fe9bfef3840f6abb46153ef97c24560b
Roman Stankiewicz <roman.stan3@gmail.com>
2017-06-26 16:35:32 +0100

    links added

--- src/js/app.js
+++ src/js/app.js
@@ -22,4 +22,18 @@ $(document).ready(() => {
 
   });
 
+
+  $("a.scroll").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      const hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - 2
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+
+
 });
